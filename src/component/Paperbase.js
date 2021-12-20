c1808386ff617b146bf573ccdaa7f81e03eea3d5
KANGSOONYONG <pliuhb@naver.com>
2021-12-20 23:38:41 +0900

    Paperbase 수정

--- src/component/Paperbase.js
+++ src/component/Paperbase.js
@@ -201,12 +201,12 @@ export default function Paperbase() {
           />
         </Box>
         <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-          <Home onDrawerToggle={handleDrawerToggle} />
-          <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
             <Routes>
               <Route path="/" element={<Content />} />
-              <Route path="/youtuber/:youtuber" element={<CodeDetail />} />
+              <Route path="/youtuber/:youtuber" element={<Home onDrawerToggle={handleDrawerToggle} />} />
             </Routes>
+          <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
+          <CodeDetail />
           </Box>
           <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
             <Copyright />
